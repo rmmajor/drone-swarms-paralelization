03ecf890ac5443bfd89c2da2961f98e78e0166ef
rmmajor <roman.yu.major@gmail.com>
2024-04-28 17:40:06 +0300

    refactored stuff

---
+++ Config.py
@@ -0,0 +1,55 @@
+SEED = None  # None if we want a random seed or a number if we want to specify a seed
+
+LOAD_MAP_FILE = False  # For loading map from a file
+
+# FILES PATH
+BASE_ROUTE = "./"
+DATA_ROUTE = "data.json"
+MAP_ROUTE = "map.txt"
+
+# Episode configuration
+SIMULATIONS = 500
+PRINT_SIMULATIONS = False  # Print for checkpoint on console
+SIMULATIONS_CHECKPOINT = (
+    100  # Number of episodes that must happen before printing checkpoint
+)
+
+# Environment configuration
+ENVIRONMENT_ROWS = 9
+ENVIRONMENT_COLUMNS = 9
+SQUARE = True  # Make environment polygon squared
+START_CORNER_0_0 = True
+
+# Agent Configuration
+ACTIONS_DICT = {
+    0: "left",
+    1: "up",
+    2: "right",
+    3: "down",
+}
+NEW_CELL_REWARD = 358.736076826821
+VISITED_CELL_REWARD = -31.1376955791041
+NO_CELL_REWARD = -225.171111437135
+
+# Learning process configuration
+GLOBAL_MODEL = False
+EPSILON = 0.468937067929711  # 0.498082374999
+MIN_EPSILON = 0.05
+GAMMA = 0.902865796260127
+MEMORY_SIZE = 60
+BATCH_SIZE = 63
+EPOCHS = 2
+VERBOSE = 0
+EPSILON_DECAY = 0.929049010143763
+
+# Model
+DENSE1_SIZE = 167
+DENSE1_ACTIVATION = "linear"
+DENSE2_SIZE = len(ACTIONS_DICT)
+DENSE2_ACTIVATION = "softmax"
+OPTIMIZER = "RMSprop"
+
+# Early experiment stopping
+MAXIMUM_WAIT_HOURS = 0.5  # wtf?
+COMPLETENESS_COVERAGE = 1.0  # float between 0 and 1  # wtf?
+MAXIMUM_UNCHANGED_ENVIRONMENT_EPISODES = 9999999999  # wtf?
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # drone-swarms-paralelization
 RHR z TRSPO
+
+
+Based on the: https://github.com/TheMVS/uav_swarm_reinforcement_learning?tab=MIT-1-ov-file
